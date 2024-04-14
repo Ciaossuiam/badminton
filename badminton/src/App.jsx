b985f371c3e8e348250ed963928e7f6abd968646
mouthleechewei@gmail.com <mouthleechewei@gmail.com>
2024-04-14 09:25:40 +0800

    導入redux-toolkit

--- badminton/src/App.jsx
+++ badminton/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-    <div>app</div>
+    <h2>app</h2>
+
     </>
   )
 }
---
+++ badminton/src/features/counter/counterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const example = createSlice({
+  name: 'counter',
+  initialState: {
+    
+  },
+  reducers: {
+  },
+})
+
+// export const {  } = counterSlice.actions
+
+export default example.reducer
